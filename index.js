064cff1830e619cf13af78ea8b93edcdd6acdf79
carlitos321 <ca.cabrera321@gmail.com>
2018-01-24 19:47:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-class BoardMember {
-  constructor(name, homeState, training) {
+function BoardMember(name, homeState, training) {
     this.name = name;
     this homeState = homeState;
     this.training = training;
