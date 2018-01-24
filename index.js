bde0ca39b331d1d321e352be0542b9c8497767b7
carlitos321 <ca.cabrera321@gmail.com>
2018-01-24 19:59:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-class BoardMember {
-  constructor(name, homeState, training) {
-    this.name = name;
-    this homeState = homeState;
-    this.training = training;
-  }
-  veto() {
-    return `No, I must disagree`
-  }
+function BoardMember(name, homeState, training) {
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
 
-  approve() {
-    return `You can do that!`
-  }
-
-  doCharity() {
-    return `I like to help people.`
-  }
-
-  releasePressStatement() {
-    return `You will see great things from Scuber.`
-  }
-
-  sayHi() {
-    return `Hi, my name is ${this.name}. I am from <homestate>, and I was
-    trained in ${this.training}.`
-  }
 }
+
+BoardMember.prototype.veto = function(){
+  return `No, I must diasagree`
+}
+
+BoardMember.prototype.approve = function(){
+  return `You can do that!`
+}
+
+BoardMember.prototype.doCharity = function(){
+  return `I like to help people.`
+}
+
+BoardMember.prototype.releasePressStatement = function(){
+  return `You will see great things from Scuber.`
+}
+
+BoardMember.prototype.sayHi = function(){
+  return `Hi, my name is ${this.name}. I am from <homestate>, and I was
+    trained in ${this.training}.`
+}
+
+
+new BoardMember()
